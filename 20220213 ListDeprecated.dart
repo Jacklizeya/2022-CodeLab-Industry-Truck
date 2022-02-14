bcb8205e1be23772bf0215924d83420e61e3cfac
Jack Zeya Li <74479553+Jacklizeya@users.noreply.github.com>
2022-02-13 17:12:57 -0800

    Create 20220213 ListDeprecated.dart

---
+++ 20220213 ListDeprecated.dart
@@ -0,0 +1,13 @@
+void main() {
+  // All of these are OK
+  List<int> listA = [3, 4, 5, 6];
+  var listB = <int>[7, 8, 9, 10];
+  List<int> listC = <int> [11, 12, 13, 14];
+
+  //This is not OK: warning list literal can not be prefixed
+  List<int> listD = List<int> [22, 23, 24, 25];
+
+  print(listA);
+  print(listB);
+  print(listC);
+}
