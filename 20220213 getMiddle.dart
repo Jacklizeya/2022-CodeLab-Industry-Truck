9b9b27e28a4c8837ca070f24d82703c7474bfc2b
Jack Zeya Li <74479553+Jacklizeya@users.noreply.github.com>
2022-02-13 21:00:36 -0800

    Create 20220213 getMiddle.dart

---
+++ 20220213 getMiddle.dart
@@ -0,0 +1,12 @@
+String getMiddle(String s) {
+  //Code goes here!
+  
+  if (s.length % 2 == 1) {
+    return s[(s.length/2).ceil() - 1];
+  } else {
+    return s.substring((s.length / 2).ceil() - 1, (s.length/ 2).ceil() + 1);
+  }
+  
+  
+  
+}
