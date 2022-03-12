a2d49b35a379c0d038bb242b63e0297b33655f41
Jack Zeya Li <74479553+Jacklizeya@users.noreply.github.com>
2022-03-12 00:05:36 -0800

    Update 20220311 AsyncAwait.dart

--- 20220311 AsyncAwait.dart
+++ 20220311 AsyncAwait.dart
@@ -1,20 +1,32 @@
 Future<int> createData () async {
-  print('Just enter the function');
+  print('Just enter the outtakes time function');
+  // do it now
+  await innerTakesTime();
+  return 1000;
+}
+
+Future<int> innerTakesTime () async {
+  print('Just enter the inner takes time function');
   // do it now
   await Future.delayed(
     Duration(seconds: 5),
     () {print('Duration is done');});
-  print('inside creating data');
   return 100;
 }
+
 
 void main () {
   // do it later
   final aFuture = createData();
+  print(aFuture);
   print('Main');
 }
 
-// Just enter the function
+// Just enter the outtakes time function
+// Just enter the inner takes time function
+// Instance of '_Future<int>'
 // Main
 // Duration is done
-// inside creating data
+
+// Conclusion: it will enter the body, and proceed as much as it can, like activating the network request, file reading...
+// It will await the result AFTER it activate that function.
