cded2fcfaa5cc4cc6a8d626423d50909be559329
Jack Zeya Li <74479553+Jacklizeya@users.noreply.github.com>
2022-03-13 09:46:41 -0700

    Update 20220311 AsyncAwait.dart

--- 20220311 AsyncAwait.dart
+++ 20220311 AsyncAwait.dart
@@ -30,3 +30,41 @@ void main () {
 
 // Conclusion: it will enter the body, and proceed as much as it can, like activating the network request, file reading...
 // It will await the result AFTER it activate that function.
+
+
+
+
+
+
+
+// There is another version, shut down operation at Main by using await
+
+Future<int> createData () async {
+  print('Just enter the outtakes time function');
+  // do it now
+  await innerTakesTime();
+  return 1000;
+}
+
+Future<int> innerTakesTime () async {
+  print('Just enter the inner takes time function');
+  // do it now
+  await Future.delayed(
+    Duration(seconds: 5),
+    () {print('Duration is done');});
+  return 100;
+}
+
+
+void main () async {
+  // do it later
+  final aFuture = await createData();
+  print(aFuture);
+  print('Main');
+}
+
+// Just enter the outtakes time function
+// Just enter the inner takes time function
+// Instance of '_Future<int>'
+// Main
+// Duration is done
