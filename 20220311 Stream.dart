351713cc300ecd29c43ad6bcd319fa421a5cb18b
Jack Zeya Li <74479553+Jacklizeya@users.noreply.github.com>
2022-04-22 23:45:50 -0700

    Update 20220311 Stream.dart

--- 20220311 Stream.dart
+++ 20220311 Stream.dart
@@ -1,23 +1,33 @@
-class NumberCreator{
+import 'dart:async';
+
+
+// When code hooks up with Timer, it generates 发报机
+// When code meets Art and Business, UX UI, it generates 产品
+
+class NumberCreator {
   var _count = 1;
-  
+  // one of its field, one of its element, Generic types
   final _controller = StreamController<int>();
-  
+
+  get stream => _controller.stream;
+
   NumberCreator() {
-   Timer.periodic(Duration(seconds: 1), (t) {
-     _controller.sink.add(_count);
-     _count++;
-   }); 
+    Timer.periodic(Duration(seconds: 1), (_) {
+      _controller.sink.add(_count);
+      _count++;
+    });
   }
 }
 
-final myStream = NumberCreator().stream;
-final subscription = myStream.listen(
-  (data) {print('Data: $data');},
-  onError: (err) {print('Error!');},
-  cancelOnError: false,
-  onDone: () {print('Done');},
-);
+void main() {
+  final myStream = NumberCreator().stream;
+
+  final mySubscription = myStream.listen((data) {
+    print(data);
+  });
+
+}
+
 
 subscription.pause();
 subscription.resume();
