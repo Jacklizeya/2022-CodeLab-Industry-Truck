7863c1433710faa7449f0a4b5b9808ba30434ca1
Jack Zeya Li <74479553+Jacklizeya@users.noreply.github.com>
2022-04-22 23:28:02 -0700

    Update and rename 20220207 super.md to 20220207 super.dart

--- 20220207 super.dart
+++ 20220207 super.dart
@@ -1,3 +1,4 @@
+/*
 Constructor for class is totally different!
 
 Constructor(a, this.b) : super(a);
@@ -7,3 +8,16 @@ In Angular I saw lots of
 
 
 subClass( this.a, this.b, this.c, d, e ): super (d, e);
+*/
+
+class EV {
+  String battery;
+
+  EV(this.battery);
+}
+
+class Tesla extends EV {
+  String laptop;
+
+  Tesla(battery, this.laptop) : super(battery);
+}
